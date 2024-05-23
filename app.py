9ecbf43a3dcbb12dede27d504593e4ac7475c02b
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-24 01:51:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def image_captions(temp, top_p):
           input={
               "top_k": 50,
               "top_p": top_p,
-              "prompt": f" Write a creative caption about only the descriptions made by this salesforce/blip model {out_blip} and do not use the word 'None' in any of your responses. ",
+              "prompt": f" Write a creative caption about only the descriptions made here {out_blip} and do not use the word 'None' in any of your responses. ",
               "temperature": temp,
               "max_new_tokens": 512,
               "min_new_tokens": 0,
