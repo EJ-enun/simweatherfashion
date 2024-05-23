4889de1c51022ed9abe7e4078c48a61b4e265058
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-24 01:03:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def image_captions(temp, top_p):
 	# Create a button for copying text
         # Display or clear chat messages
         for message in st.session_state.messages:
-            with st.chat_message(message["role"]]):
+            with st.chat_message(message["role"]):
                 st.write(message[event_str])
 
   
