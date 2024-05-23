e2c349f5f159487005c4401f88760a972be476a8
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-24 01:35:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,7 @@ def image_captions(temp, top_p):
           input={
               "top_k": 50,
               "top_p": top_p,
-              "prompt": f" Write a social media post caption using {out_blip} do not use the word 'None' in any of your responses",
+              "prompt": f" Write a caption using only {out_blip} and do not use the word 'None' in any of your responses. Keep all responses in a straight line. ",
               "temperature": temp,
               "max_new_tokens": 512,
               "min_new_tokens": 0,
