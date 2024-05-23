cf5553b871d23a54f5202d67e381ea6cc0dd65ff
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-24 01:29:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -264,9 +264,9 @@ def image_captions(temp, top_p):
 
         # Display or clear chat messages
         for message in st.session_state.messages:
-	    st.session_state.messages = [{"role": "assistant", "content":str(event)}]
-    	        with st.chat_message(message["role"]):
-        	        st.write(message["content"])
+            st.session_state.messages = [{"role": "assistant", "content":str(event)}]
+    	    with st.chat_message(message["role"]):
+                st.write(message["content"])
 
 
 
