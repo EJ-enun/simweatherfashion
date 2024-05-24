708b82a4e4d6fc4538f45ccb2c4c997ccbf883d8
EJay Enun <68190790+EJ-enun@users.noreply.github.com>
2024-05-24 13:30:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -216,7 +216,7 @@ def arctic_gen(weather, options):
     },
 ): yield str(event)
 def gen_image_from_arctic_prompt(prompt):
-    st.input(prompt, text)
+    st.text_input(prompt, text)
     if st.button("Generate with Arctic"):
 	    
         try:
